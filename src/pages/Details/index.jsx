88550e54909fbc007e75b5dbc315d94f42f773bb
axel <axelselbach90@gmail.com>
2023-10-23 12:21:13 -0300

    Created Tag component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,6 +2,7 @@ import { Container, Links } from './styles'
 import { Header } from '../../components/Header' 
 import { Button } from '../../components/Button'
 import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
 
 export function Details() {
   return (
@@ -9,13 +10,14 @@ export function Details() {
       <Header />
       <Section title="Links úteis">
         <Links>
-          <li>
-            <a href="#">https://rocketseat.com.br</a>
-          </li>
-          <li>
-            <a href="#">https://rocketseat.com.br</a>
-          </li>
+          <li><a href="#">https://rocketseat.com.br</a></li>
+          <li><a href="#">https://rocketseat.com.br</a></li>
         </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="nodejs" />
       </Section>
 
       <Button title="Voltar"/>
