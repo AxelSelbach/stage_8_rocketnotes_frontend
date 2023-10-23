8300564cefa5cb67147dc000bfd42e277c5c0642
axel <axelselbach90@gmail.com>
2023-10-23 20:27:49 -0300

    started signin page

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import theme from './styles/theme'
-import GlobalStyles from './styles/global'
 import { ThemeProvider } from 'styled-components'
-import { Home } from './pages/Home'
+import { SignIn } from './pages/SignIn'
+import GlobalStyles from './styles/global'
+import theme from './styles/theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
