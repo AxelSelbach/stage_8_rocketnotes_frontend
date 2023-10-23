755b26798b5e5b1de6ab6951f3bd23956008b8ac
axel <axelselbach90@gmail.com>
2023-10-22 21:46:07 -0300

    logout button and added react icons

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -41,5 +41,14 @@ align-items: center;
     color: ${({ theme }) => theme.COLORS.WHITE};
   }
 }
+`;
 
+export const Logout = styled.button`
+  border: none;
+  background: none;
+
+  > svg {
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+    font-size: 36px;
+  }
 `
