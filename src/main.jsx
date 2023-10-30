c7e8157837d40f4924d6c150bb2702811d2b2fe7
axel <axelselbach90@gmail.com>
2023-10-30 14:41:47 -0300

    feat: created context

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ThemeProvider } from 'styled-components'
-import { Routes } from './routes'
-import GlobalStyles from './styles/global'
 import theme from './styles/theme'
+import ReactDOM from 'react-dom/client'
+import GlobalStyles from './styles/global'
+import { Routes } from './routes'
+import { MyContext } from './myContext'
+import { ThemeProvider } from 'styled-components'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Routes />
+      <MyContext.Provider value={{ email: 'axel@gmail.com' }}>
+        <Routes />
+      </MyContext.Provider>
     </ThemeProvider>
   </React.StrictMode>,
 )
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,11 +1,18 @@
-import { Container, Form, Background } from './styles';
-import { Input } from '../../components/Input';
-import { Button } from '../../components/Button';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { MyContext } from '../../myContext';
+import { Input } from '../../components/Input';
+import { FiMail, FiLock } from 'react-icons/fi';
+import { Button } from '../../components/Button';
+import { Container, Form, Background } from './styles';
 
-import { FiMail, FiLock } from 'react-icons/fi'
+
 
 export function SignIn() {
+  
+  const data = useContext(MyContext)
+  console.log("Meu contexto =>", data)
+
   return ( 
     <Container>
       <Form>
